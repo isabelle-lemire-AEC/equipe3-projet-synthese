d0b55abd1b8a4de7f0073a3c58b7c6dfa797f30d
carocadieux <caroline-cadieux@outlook.com>
2024-03-08 16:22:28 -0500

    page offre stage et offre stage detail

--- src/views/OffresStages.vue
+++ src/views/OffresStages.vue
@@ -1,5 +1,36 @@
 <template>
+    <h1>Offres de stage</h1>
 
+    <button>Ajouter une offre de stage</button>
+
+    <table>
+        <thead>
+            <tr>
+                <th scope="col">Poste</th>
+                <hr>
+                <th scope="col">Secteur d'activité</th>
+                <hr>
+                <th scope="col">Région</th>
+                <hr>
+                <th scope="col">Date de publication</th>
+                <hr>
+                <th scope="col"></th>
+            </tr>
+        </thead>
+
+        <tbody>
+            <th>
+                <!--icone-->
+                <h4>Intégrateur Web</h4>
+                <p>Acolyte communication</p>
+
+            </th>
+            <td>Communication</td>
+            <td>Mauricie</td>
+            <td>2023-03-03</td>
+            <td><!--icone--></td>
+        </tbody>
+    </table>
 </template>
 
 <script setup>
