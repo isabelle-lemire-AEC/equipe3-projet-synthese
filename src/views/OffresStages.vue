d1673195978892069586c130391cf61b571740ee
carocadieux <caroline-cadieux@outlook.com>
2024-03-12 19:25:14 -0400

    debut style

--- src/views/OffresStages.vue
+++ src/views/OffresStages.vue
@@ -1,39 +1,44 @@
 <template>
+    <div class="container">
     
-    <h1>Offres de stage</h1>
+        <h1>Offres de stage</h1>
 
-    <button>Ajouter une offre de stage</button>
+        <button>Ajouter une offre de stage</button>
 
-    <table>
-        <thead>
-            <tr>
-                <th scope="col">Poste</th>
-                
-                <th scope="col">Secteur d'activité</th>
-                
-                <th scope="col">Région</th>
-                
-                <th scope="col">Date de publication</th>
-                
-                <th scope="col"></th>
-            </tr>
-        </thead>
-   
-        <tbody>
-            <th>
-                <!--icone-->
-                <h4>Intégrateur Web</h4>
-                <p>Acolyte communication</p>
+        <div class="ofrreStage">
 
-            </th>
-            <td>Communication</td>
-            <td>Mauricie</td>
-            <td>2023-03-03</td>
-            <td><!--icone--></td>
-        </tbody>
+            <table>
+                <thead>
+                    <tr>
+                        <th scope="col">Poste</th>
+                        
+                        <th scope="col">Secteur d'activité</th>
+                        
+                        <th scope="col">Région</th>
+                        
+                        <th scope="col">Date de publication</th>
+                        
+                        <th scope="col"></th>
+                    </tr>
+                </thead>
         
-    </table>
-    <hr>
+                <tbody>
+                    <th>
+                        <!--icone-->
+                        <h4>Intégrateur Web</h4>
+                        <p>Acolyte communication</p>
+
+                    </th>
+                    <td>Communication</td>
+                    <td>Mauricie</td>
+                    <td>2023-03-03</td>
+                    <td><!--icone--></td>
+                </tbody>
+                
+            </table>
+            <hr>
+        </div>
+    </div>
 </template>
 
 <script setup>
@@ -44,6 +49,13 @@
 
 </script>
 
-<style>
+<style scoped>
+h1{
+    color: #707070;
+    padding: 60px;
+}
+
+
+
 
 </style>
