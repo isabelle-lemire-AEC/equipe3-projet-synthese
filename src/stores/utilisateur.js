ec7b8b450b2f469ee971f95b76ac0ab6e87e0daa
isabelle-lemire-AEC <isabelle.lemire.02@edu.cegeptr.qc.ca>
2024-03-24 14:24:48 -0400

    essaies pour validation - non terminer

---
+++ src/stores/utilisateur.js
@@ -0,0 +1,25 @@
+// utilisateur.js
+
+import { defineStore } from 'pinia';
+
+export const utilisateurStore = defineStore({
+  id: 'utilisateur',
+  state: () => ({
+    nomPrenom: '',
+    courriel: ''
+  }),
+  actions: {
+    updateInfo(newNomPrenom, newCourriel) {
+      console.log('Updating user info...');
+      console.log('New nomPrenom:', newNomPrenom);
+      console.log('New courriel:', newCourriel);
+
+      this.nomPrenom = newNomPrenom;
+      this.courriel = newCourriel;
+
+      console.log('User info updated successfully!');
+      console.log('NomPrenom:', this.nomPrenom);
+      console.log('Courriel:', this.courriel);      
+    }
+  }
+});
