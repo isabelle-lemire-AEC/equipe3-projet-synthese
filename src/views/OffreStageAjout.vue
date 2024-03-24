39b8c0efe728c82a6bf8db032bdd18d1b575617e
carocadieux <caroline-cadieux@outlook.com>
2024-03-23 21:46:13 -0400

    ajustement page offre de stage ajout avec le code de Raph

--- src/views/OffreStageAjout.vue
+++ src/views/OffreStageAjout.vue
@@ -1,79 +1,108 @@
 
 
 <template>
-    <div class=" h-2/3 bg-neutral-500 felex flex-col w-96 ">
-      <form class="flex flex-col" id="ajout-intern-sheep-offer" @submit.prevent="submitForm">
-                <div class="my-4">
-          <!-- ton annuler nest pas définie raph -->
-                <button class="annuler" type="submit" @click="annulerAjout">Annuler</button>
-                <button class="mettre-a-jour" type="submit"><i class="fas fa-save"></i>Sauvegarder</button>
-            </div>
-             <input class="my-4" v-model="offerData.title" type="text" placeholder="Titre de l'offre" />
-  
-        <textarea v-model="offerData.description" placeholder="Description"></textarea>
-          <select class="my-4" v-model="offerData.province._id">
-          <option disabled value="">Sélectionnez une province</option>
-          <option v-for="province in provinces" :key="province._id" :value="province._id">{{ province.value }}</option>
-        </select>
-
-        <select class="my-4" v-model="offerData.internshipType._id">
-          <option disable value="">select Type temps plein ou partiel</option>
-          <option v-for="internshipType in  internshipTypes" :key="internshipType._id" :value="internshipType._id">{{ internshipType.value }}</option>
-        </select>
-  
-       <div class="my-4">
-        <input class="my-4" v-model="offerData.startDate" type="date" placeholder="Date de début" />
-        <input v-model="offerData.endDate" type="date" placeholder="Date de fin" />
-      </div>
     <section class="entete">
-        <h3>Ajouter une offre de stage</h3>
+        <h3>Offre de stage</h3>
     </section>
-</form>
-    <form @submit.prevent="soumettreFormulaire">
 
-        <button type="submit" @click="annulerAjout">Annuler</button>
-        
-        <button type="submit">Sauvegarder</button>
-        
+    <div>
+      <form  id="ajout-intern-sheep-offer" @submit.prevent="submitForm">
+                
+          <!-- ton annuler nest pas définie raph -->
+            <button class="annuler" type="submit" @click="annulerAjout">Annuler</button>
 
-        <div class="groupe-titre-entreprise">
-            <h3>Titre:</h3>
+            <button class="ajout" type="submit"><i class="fas fa-save"></i>Sauvegarder</button>
             
-              <input type="text" id="titre" v-model.trim="offre.title">
-              <p class="validForm" v-if="erreurs.title">Veuillez remplir ce champ</p>
-            
-            <h3>Entreprise:</h3>
-            <label for="type">Veuillez effectuer un choix</label>
-           
-            <select name="entreprise" id="entreprise" v-model="offre.enterprise.name">
-                <option value="" disabled selected>Choisir une entreprise</option>
-                <option v-for="enterprise in entreprises" :key="enterprise._id" :value="enterprise.name">
-                {{ enterprise.name }}
-                </option>
-            </select>
-            <p class="validForm" v-if="erreurs.name">Veuillez effectuer un choix</p>
+            <div class="groupe-titre-entreprise">
+                <h3>Titre:</h3>
 
-        </div>
+                <input v-model.trim="offerData.title" type="text" placeholder="Titre de l'offre" />
+                <p class="validForm" v-if="erreurs.title">Veuillez remplir ce champ</p>
 
-      <div class="my-4 text-red-400 flex flex-col">
-        <label for="weeklyWorkHours">Heures de travail par semaine :</label>
-        <input v-model.number="offerData.weeklyWorkHours" type="number" placeholder="Heures de travail par semaine" />
-      </div>
-       
-        <div class="flex flex-col my-4">
-          <label class="text-red-500" for="Salaire">Salair / demaine; </label>
-        <input v-model.number="offerData.salary" type="number" placeholder="Salaire" />
-        </div>
-      
-        <input  class="my-4" v-model="offerData.requiredSkills" type="text" placeholder="Compétences requises" />
-  
-        <select  class="my-4" v-model="offerData.enterprise">
-           <option disabled value="">Sélectionnez une entreprise</option>
-            <option v-for="entreprise in entreprises" :key="entreprise._id" :value="entreprise">
-           {{ entreprise.name }} 
-          </option>
-        </select>
-        <button class="p-4 bg-red-400 rounded-lg mx-4" type='submit'>Ajouter offre</button>
+                <h3>Entreprise:</h3>
+                <label for="type">Veuillez effectuer un choix</label>
+
+                <select v-model="offerData.enterprise">
+                    <option disabled value="">Sélectionnez une entreprise</option>
+                    <option v-for="entreprise in entreprises" :key="entreprise._id" :value="entreprise">
+                        {{ entreprise.name }} 
+                    </option>
+                </select>
+                <p class="validForm" v-if="erreurs.name">Veuillez effectuer un choix</p>
+            </div>
+
+            <div class="groupe-tache">
+                <h1>Description de la tâche</h1>
+                <textarea v-model.trim="offerData.description" placeholder="Description"></textarea>
+                <p class="validForm" v-if="erreurs.description">Veuillez remplir ce champ</p>
+            </div>
+
+            <div class="groupe-programme">
+                <h3>Programme de formation</h3>
+                <input type="text" id="ajout-programme" v-model.trim="offerData.enterprise.activitySector">
+                <p class="validForm" v-if="erreurs.activitySector">Veuillez remplir ce champ</p>
+            </div>
+
+            <div class="groupe-exigence">
+                <h3>Exigences</h3>
+                <input v-model.trim="offerData.requiredSkills" type="text" placeholder="Compétences requises" />
+                <p class="validForm" v-if="erreurs.requiredSkills">Veuillez remplir ce champ</p>
+            </div>
+
+
+            <div class="groupe-info-stage">
+                <h3>Informations sur le stage recherché</h3>
+
+                <div>
+                    <h3>Type de stage</h3>
+                    <label for="ajout-type">Veuillez effectuer un choix</label>
+                    <select v-model="offerData.internshipType._id">
+                        <option disable value="">select Type temps plein ou partiel</option>
+                        <option v-for="internshipType in  internshipTypes" :key="internshipType._id" :value="internshipType._id">{{ internshipType.value }}</option>
+                    </select>
+                    <p class="validForm" v-if="erreurs.internshipType">Veuillez effectuer un choix</p>
+
+                    <h3>Nombre d'heures par semaine</h3>
+                    <label for="weeklyWorkHours">Heures de travail par semaine :</label>
+                    <input v-model.number="offerData.weeklyWorkHours" type="number" placeholder="Heures de travail par semaine" />
+                    <p class="validForm" v-if="erreurs.weeklyWorkHours">Veuillez effectuer un choix</p>
+                
+                    <h3>Rénumération</h3>
+                    <input type="checkbox" id="ajout-remuneration1" name="ajout-remuneration1" value="discuter">
+                    <label for="ajout-remuneration1"> À discuter</label><br>
+                    <input type="checkbox" id="ajout-remuneration2" name="ajout-remuneration2" value="remunere">
+                    <label for="ajout-remuneration2"> Stage rémunéré</label><br>
+                    <input type="checkbox" id="ajout-remuneration3" name="ajout-remuneration3" value="nonRemunere">
+                    <label for="ajout-remuneration3"> Stage non rémunéré</label>
+                    <label for="Salaire">Salaire / semaine; </label>
+                    <p class="validForm" v-if="erreurs.salary">Veuillez effectuer un choix</p>
+                    <!--<input v-model.number="offerData.salary" type="number" placeholder="Salaire" />-->
+                </div>
+
+                <div>
+                    <h3>Date de début</h3>
+                    <input v-model="offerData.startDate" type="date" placeholder="Date de début" />
+                    <p class="validForm" v-if="erreurs.startDate">Veuillez effectuer un choix</p>
+
+                    <h3>Date de fin</h3>
+                    <input v-model="offerData.endDate" type="date" placeholder="Date de fin" />
+                    <p class="validForm" v-if="erreurs.endDate">Veuillez effectuer un choix</p>
+
+                    <select v-model="offerData.province._id">
+                        <option disabled value="">Sélectionnez une province</option>
+                        <option v-for="province in provinces" :key="province._id" :value="province._id">{{ province.value }}</option>
+                    </select>
+                </div>
+                
+            </div>
+
+            <section class="info-sup">
+                <h3>Informations suplémentaires</h3>
+                <textarea id="ajout-info-sup-form"></textarea>
+            </section>
+
+
+        <!--<button class="p-4 bg-red-400 rounded-lg mx-4" type='submit'>Ajouter offre</button>-->
       </form>
     </div>
   </template>
@@ -85,10 +114,12 @@ import { ref, onMounted } from 'vue';
 import { useEntreprise } from '../composables/offres_stage/stageEntreprise';
 import { fetchProvinces } from '@/composables/api';
 import { fetchStageTypes } from '@/composables/api';
+import { useRouter } from 'vue-router';
 
 // type RAPH***
 import { useInternshipOffers } from '../composables/offres_stage/offreDeStage';
 
+const router = useRouter();
 const { ajouterOffre } = useInternshipOffers();
 const { getAllEntreprises, response: entreprisesResponse, error: entreprisesError, loading: entrepriseLoading} = useEntreprise();
 
@@ -137,28 +168,107 @@ onMounted(async () => {
 });
 
 const offerData = ref({
-  title: "",
-  description: "",
+  title: '',
+  description: '',
   //est ce que l'entreprise à été transformeé en id ?
-  enterprise: { _id: "" }, 
-  startDate: "",
-  endDate: "",
+  enterprise: { _id: '' }, 
+  startDate: '',
+  endDate: '',
   weeklyWorkHours: 0,
   salary: 0,
-  province: { _id: "" },
+  province: {  _id: '', value: '' },
   //attention ici verifier si je peux mettre plus d'un string dans le tableau de skills
   requiredSkills: [],
-  internshipType: { _id: "" },
+  internshipType: { _id: '', value: '' },
   paid: "DISCRETIONARY",
   isActive: true
 });
 
-const submitForm = async () => {
-  console.log(offerData.value)
-  await ajouterOffre(offerData.value);
-   console.log("Offre ajoutée avec succès");
+// validation formulaire
 
+const erreurs = ref({
+        title: false,
+        name: false,
+        description: false,
+        activitySector: false,
+        startDate: false,
+        endDate: false,
+        requiredSkills: false,
+        internshipType: false,
+        weeklyWorkHours: false,
+        salary: false,
+    });
+
+   
+const validerFormulaire = () => {
+  
+    erreurs.value.title= offerData.value.title === '',
+    erreurs.value.name= offerData.value.enterprise === '',
+    erreurs.value.description= offerData.value.description === '',
+    erreurs.value.activitySector= offerData.value.enterprise.activitySector.value === '',
+    erreurs.value.startDate= offerData.value.startDate === '',
+    erreurs.value.endDate= offerData.value.endDate ==='',
+    erreurs.value.requiredSkills= offerData.value.requiredSkills === '',
+    erreurs.value.internshipType= offerData.value.internshipType.value === '',
+    erreurs.value.weeklyWorkHours= offerData.value.weeklyWorkHours === 0
+    erreurs.value.salary= offerData.value.salary === 0
+  
+    console.log("Erreurs :", erreurs.value);
+  // Vérifie s'il y a des erreurs dans le formulaire
+  return Object.values(erreurs.value).some(err => err);
 };
+
+
+const formulaireValide = ref(false);
+
+const submitForm = async () => {
+    try{
+        formulaireValide.value = validerFormulaire();
+        if (!formulaireValide.value) {
+            await ajouterStage();
+            //console.log(offerData.value)
+            console.log("Offre ajoutée avec succès");
+        } else {
+            throw new Error("Veuillez remplir tous les champs obligatoires."); 
+          }
+    } catch (error) {
+            console.error("Erreur lors de la soumission du formulaire :", error);
+            }
+};
+
+
+const ajouterStage = async () => {
+    try {
+            if (validerFormulaire()) {
+                throw new Error("Veuillez remplir tous les champs obligatoires.");
+            }
+            console.log("Tentative d'ajout de l'offre :", offre.value);
+            await ajouterOffre(offerData.value);
+            console.log("Nouvelle offre ajoutée");
+            router.push({ name: 'OffresStages' });
+        } catch (error) {
+            console.error("Erreur lors de l'ajout de l'offre de stage :", error);
+        }
+
+ 
+};
+
+
+
+const annulerAjout = () => {
+        console.log("Annuler l'ajout de l'offre");
+        router.push({ name: 'OffresStages' });
+}
+ 
+  
+   
+
+
+
+
+
+
+
 </script>
 
 <style></style>
