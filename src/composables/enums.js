b3fdf0a9c91c9e5dcd6080481dc3c6cb1d0022fe
thiviergejonathan <thiviergejonathan@gmail.com>
2024-03-28 20:28:05 -0400

    bouton valider les demandes/offres

--- src/composables/enums.js
+++ src/composables/enums.js
@@ -1,7 +1,4 @@
-// Ici je définis une ÉNUMÉRATION pour vérifier sur quel page l'utilisateur se situe.
-// Cette ÉNUM est utilisé pour le composant 'ElementListeStage'.
-export const page = Object.freeze({
-    demandesStages: 'DemandesStages',
-    offresStages: 'OffresStages',
-    tableauDeBord: 'TableauDeBord'
+// Ici je définis une ÉNUMÉRATION
+export const abc = Object.freeze({
+    //todo pour 'paid'?
 })
