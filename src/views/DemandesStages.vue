69c0ee74be4cf03ef6ea47a294ec4bc2ea955061
isabelle-lemire-AEC <isabelle.lemire.02@edu.cegeptr.qc.ca>
2024-04-01 20:39:00 -0400

    débuter page demande stage

--- src/views/DemandesStages.vue
+++ src/views/DemandesStages.vue
@@ -1,62 +1,77 @@
 <template>
-    <div>
+    <div class="demandestages-list">
         <h1>Demandes de stage</h1>
-        <RouterLink to="/demande-de-stage-ajout">
-            <button class="bouton bouton--jaune">Ajouter une demande</button>
-        </RouterLink>
-        <div class="listeStages">
-            <div class="liste-header">
-                <span>Poste</span>
-                <span>Secteur d'activité</span>
-                <span>Région</span>
-                <span>Date d'inscription</span>
-            </div>
-            <ElementListeStage v-for="demande in toutesDemandes" 
-                :key="demande._id" 
-                :posteTitre="demande.title" 
-                :posteNom="demande.candidate.firstName+' '+demande.candidate.lastName" 
-                :info2="secteurActivite.value"
-                :region="demande.province.value" 
-                :date="demande.startDate" 
-                :id="demande._id" 
-                :isDemande="true"
-                :isTableauDeBord="false"
-                :isActive="demande.isActive"></ElementListeStage>
+        <button class="bouton bouton--jaune" @click="goToDemandesAjout">Ajouter une demande</button>
+
+        <div class="candidats__liste">
+            <div class="form-fiche__encadre"> 
+                <div>
+                    <div >
+                        <span>Poste</span>
+                        <span>Secteur d'activité</span>
+                        <span>Région</span>
+                        <span>Date d'inscription</span>
+                    </div>
+                    <ElementListeStage v-for="demande in toutesDemandes" 
+                        :key="demande._id" 
+                        :posteTitre="demande.title" 
+                        :posteNom="demande.candidate.firstName+' '+demande.candidate.lastName" 
+                        :info2="secteurActivite.value"
+                        :region="demande.province.value" 
+                        :date="demande.startDate" 
+                        :id="demande._id" 
+                        :isDemande="true"
+                        :isTableauDeBord="false"
+                        :isActive="demande.isActive"></ElementListeStage>
+                </div>
+            </div>            
         </div>
+
+
     </div>
 </template>
 
 <script setup>
-import ElementListeStage from '../components/ElementListeStage.vue'
-import { useInternshipRequests } from '../composables/demandes_stages/demandeDeStage.js'
-import { useCandidat } from '../composables/candidats/candidat.js'
-import { useProvinces } from '../composables/provinces/provinces.js'
-import { useInternshipTypes } from '@/composables/types_stage/types_stage.js'
-import { ref, onMounted } from 'vue';
-import { useActivitySectors } from '../composables/secteurs_activites/secteurs_activites.js'; 
+    import { useInternshipRequests } from '../composables/demandes_stages/demandeDeStage.js'
+    import { useCandidat } from '../composables/candidats/candidat.js'
+    import { useProvinces } from '../composables/provinces/provinces.js'
+    import { useInternshipTypes } from '@/composables/types_stage/types_stage.js'
+    import { ref, onMounted } from 'vue';
+    import { useActivitySectors } from '../composables/secteurs_activites/secteurs_activites.js'; 
+    import { useRouter } from 'vue-router';
 
-const { getAllRequests, editRequest } = useInternshipRequests();
-const { getAllCandidats } = useCandidat();
-const { getAllProvinces } = useProvinces();
-const { getAllInternshipTypes } = useInternshipTypes();
-const { getAllActivitySectors } = useActivitySectors();
+    const router = useRouter();
 
-const toutesDemandes = ref([]);
-const secteursActivites = ref([]);
-const secteurActivite = ref(null);
+    const { getAllRequests, editRequest } = useInternshipRequests();
+    const { getAllCandidats } = useCandidat();
+    const { getAllProvinces } = useProvinces();
+    const { getAllInternshipTypes } = useInternshipTypes();
+    const { getAllActivitySectors } = useActivitySectors();
 
-onMounted(async () => {
-    secteursActivites.value = await getAllActivitySectors();
-    secteurActivite.value = secteursActivites.value[Math.floor(Math.random() * secteursActivites.value.length)];
+    import ElementListeStage from '../components/elementListeStage.vue'
 
-    try {
-        const response = await getAllRequests();
-        toutesDemandes.value = response.data;
-    } catch (error) {
-        console.error("Error:", error.response ? error.response.data : error.message);
-    }
-});
+    const toutesDemandes = ref([]);
+    const secteursActivites = ref([]);
+    const secteurActivite = ref(null);
 
+    // Fonction pour naviguer vers la page d'ajout de demandes de stage
+    const goToDemandesAjout = () => {
+        router.push({
+        name: 'DemandeStageAjout'
+        });
+    };
+    
+    onMounted(async () => {
+        secteursActivites.value = await getAllActivitySectors();
+        secteurActivite.value = secteursActivites.value[Math.floor(Math.random() * secteursActivites.value.length)];
+
+        try {
+            const response = await getAllRequests();
+            toutesDemandes.value = response.data;
+        } catch (error) {
+            console.error("Error:", error.response ? error.response.data : error.message);
+        }
+    });
 </script>
 
 <style scoped>
