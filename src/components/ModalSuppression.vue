d69e5ec6631c2d43f0ad860941571f69fbb3d6e5
isabelle-lemire-AEC <isabelle.lemire.02@edu.cegeptr.qc.ca>
2024-04-03 18:40:48 -0400

    modal supression aide Donaria

--- src/components/ModalSuppression.vue
+++ src/components/ModalSuppression.vue
@@ -1,38 +1,31 @@
-<!-- Modal de confirmation de suppression -->
+<!-- ModalSuppression.vue -->
 <template>
-  <div class="modal" v-if="show">
-    <div class="modal__contenu">
+  <div class="modal" v-if="showConfirmationModal">
+    <div class="modalcontenu">
       <p>{{ message }}</p>
-      <div class="modal__boutons">
-        <button class="bouton bouton--rouge" @click="annulerSuppression">
-          Annuler
-        </button>
-        <button class="bouton bouton--vert" @click="confirmerSuppression">
-          Confirmer
-        </button>
+      <div class="modalboutons">
+        <button class="bouton bouton--rouge" @click="annulerSuppression">Annuler</button>
+        <button class="bouton bouton--vert" @click="confirmerSuppression">Confirmer</button>
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref, defineProps } from "vue";
+  import { defineProps, ref, defineEmits } from 'vue';
 
-const props = defineProps({
-  message: String,
-  onConfirm: Function,
-  onCancel: Function,
-});
+  const props = defineProps({
+    showConfirmationModal: Boolean,
+    message: String,
+  });
 
-const showConfirmationModal = ref(false);
+  const emit = defineEmits(['annulerSuppression', 'confirmerSuppression']);
 
-const annulerSuppression = () => {
-  showConfirmationModal.value = false;
-  if (props.onCancel) props.onCancel();
-};
+  const annulerSuppression = () => {
+    emit('annulerSuppression');
+  };
 
-const confirmerSuppression = () => {
-  showConfirmationModal.value = false;
-  if (props.onConfirm) props.onConfirm();
-};
+  const confirmerSuppression = () => {
+    emit('confirmerSuppression');
+  };
 </script>
