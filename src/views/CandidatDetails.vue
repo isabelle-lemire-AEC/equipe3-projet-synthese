2fcd46db7211be18bb20122c732dbe0950ba274d
Anne Massicotte <anne.massicotte@hotmail.com>
2024-04-03 22:25:04 -0400

    esapcement bouton encadré candidat et offre

--- src/views/CandidatDetails.vue
+++ src/views/CandidatDetails.vue
@@ -21,44 +21,44 @@
           <i class="fas fa-square-xmark"></i>
         </button>
       </div>
-    </div>
 
-    <div class="form-fiche__encadre">
-      <!-- Section de description du candidat -->
-      <div class="form-fiche__wrapper-titre-groupe-inputs">
-        <h2>Courte présentation</h2>
-        <p>{{ candidat.description }}</p>
-      </div>
-      <!-- Section d'informations personnelles du candidat -->
-      <div class="form-fiche__wrapper-titre-groupe-inputs">
-        <h3>Informations personnelles</h3>
-        <div class="form-fiche__colonnes-inputs">
-          <div class="form-fiche__colonne-inputs">
-            <div class="form-fiche__label-input-vertical">
-              <h4>Adresse</h4>
-              <p>{{ candidat.address }}</p>
+      <div class="form-fiche__encadre">
+        <!-- Section de description du candidat -->
+        <div class="form-fiche__wrapper-titre-groupe-inputs">
+          <h2>Courte présentation</h2>
+          <p>{{ candidat.description }}</p>
+        </div>
+        <!-- Section d'informations personnelles du candidat -->
+        <div class="form-fiche__wrapper-titre-groupe-inputs">
+          <h3>Informations personnelles</h3>
+          <div class="form-fiche__colonnes-inputs">
+            <div class="form-fiche__colonne-inputs">
+              <div class="form-fiche__label-input-vertical">
+                <h4>Adresse</h4>
+                <p>{{ candidat.address }}</p>
+              </div>
+              <div class="form-fiche__label-input-vertical">
+                <h4>Ville</h4>
+                <p>{{ candidat.city }}</p>
+              </div>
+              <div class="form-fiche__label-input-vertical">
+                <h4>Province</h4>
+                <p>{{ candidat.province.value }}</p>
+              </div>
+              <div class="form-fiche__label-input-vertical">
+                <h4>Code postal</h4>
+                <p>{{ candidat.postalCode }}</p>
+              </div>
             </div>
-            <div class="form-fiche__label-input-vertical">
-              <h4>Ville</h4>
-              <p>{{ candidat.city }}</p>
-            </div>
-            <div class="form-fiche__label-input-vertical">
-              <h4>Province</h4>
-              <p>{{ candidat.province.value }}</p>
-            </div>
-            <div class="form-fiche__label-input-vertical">
-              <h4>Code postal</h4>
-              <p>{{ candidat.postalCode }}</p>
-            </div>
-          </div>
-          <div class="form-fiche__colonne-inputs">
-            <div class="form-fiche__label-input-vertical">
-              <h4>Téléphone</h4>
-              <p>{{ candidat.phone }}</p>
-            </div>
-            <div class="form-fiche__label-input-vertical">
-              <h4>Courriel</h4>
-              <p>{{ candidat.email }}</p>
+            <div class="form-fiche__colonne-inputs">
+              <div class="form-fiche__label-input-vertical">
+                <h4>Téléphone</h4>
+                <p>{{ candidat.phone }}</p>
+              </div>
+              <div class="form-fiche__label-input-vertical">
+                <h4>Courriel</h4>
+                <p>{{ candidat.email }}</p>
+              </div>
             </div>
           </div>
         </div>
